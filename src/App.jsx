1e2cfe4403643047d5b8337b29bb097dbc48533f
yuen0 <nicolaslopezpp@gmail.com>
2023-04-30 22:54:36 +0200

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import HamburgerMenu from "./Components/Navbar";
 import Footer from "./Components/Footer";
 const App = () => {
 	return (
-		<Router basename="{process.env.PUBLIC_URL}">
+		<Router>
 			<HamburgerMenu />
 			<Routes>
 				<Route exact path="/" element={<Homepage />} />
