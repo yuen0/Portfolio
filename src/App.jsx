beadd9e6263adb2436dabbc85d24988a8f0417c0
yuen0 <nicolaslopezpp@gmail.com>
2023-04-30 23:32:50 +0200

    added basename prop to router for testing

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import HamburgerMenu from "./Components/Navbar";
 import Footer from "./Components/Footer";
 const App = () => {
 	return (
-		<Router>
+		<Router basename="/Portfolio">
 			<HamburgerMenu />
 			<Routes>
 				<Route exact path="/" element={<Homepage />} />
